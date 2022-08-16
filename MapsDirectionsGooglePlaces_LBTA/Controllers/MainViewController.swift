3e5043e358241f43dc669162f6e1cb3eb423fac9
kokosDev <maks.kokos.dev@gmail.com>
2022-08-16 12:16:29 +0300

    setup region

--- MapsDirectionsGooglePlaces_LBTA/Controllers/MainViewController.swift
+++ MapsDirectionsGooglePlaces_LBTA/Controllers/MainViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import MapKit
 import LBTATools
+import SwiftUI
 
 class MainViewController: UIViewController {
     
@@ -15,11 +16,36 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.addSubview(mapView)
         mapView.fillSuperview()
+        setupRegionForMap()
+    }
+    
+    fileprivate func setupRegionForMap() {
+        let coordinate = CLLocationCoordinate2D(latitude: 59.931505, longitude: 30.364036)
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let region  = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
 
-        mapView.mapType = .hybridFlyover
+
+// MARK: - SwiftUI Preview
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> MainViewController {
+            return MainViewController()
+        }
         
+        func updateUIViewController(_ uiViewController: MainViewController, context: Context) {
+            
+        }
+        
+        typealias UIViewControllerType = MainViewController
     }
 }
