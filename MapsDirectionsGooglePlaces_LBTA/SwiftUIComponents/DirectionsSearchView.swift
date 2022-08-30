ad06ae23f89dcfcb555b36e636461c35ea0b4a2b
kokosDev <maks.kokos.dev@gmail.com>
2022-08-30 18:57:39 +0300

    implemented Navigation Link

--- MapsDirectionsGooglePlaces_LBTA/SwiftUIComponents/DirectionsSearchView.swift
+++ MapsDirectionsGooglePlaces_LBTA/SwiftUIComponents/DirectionsSearchView.swift
@@ -20,50 +20,76 @@ struct DirectionsMapView: UIViewRepresentable {
     }
 }
 
-struct DirectionsSearchView: View {
+struct SelectLocationView: View {
+    
+    @Binding var isShowing: Bool
+    
     var body: some View {
-        ZStack(alignment: .top) {
-            VStack(spacing: -4) {
-                VStack {
-                    
-                    HStack(spacing: 16) {
-                        Image(uiImage: #imageLiteral(resourceName: "start_location_circles")).frame(width: 24, height: 24)
-                        HStack {
-                            Text("Source")
-                            Spacer()
-                        }.padding()
-                        .background(Color.white)
-                        .cornerRadius(3)
+        VStack {
+            Button(action: {
+                self.isShowing = false
+            }, label: {
+                Text("Dismiss")
+            })
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct DirectionsSearchView: View {
+    
+    @State var isSelectingSource = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack(alignment: .top) {
+                VStack(spacing: -4) {
+                    VStack {
                         
+                        HStack(spacing: 16) {
+                            Image(uiImage: #imageLiteral(resourceName: "start_location_circles")).frame(width: 24, height: 24)
+                            NavigationLink(
+                                destination: SelectLocationView(isShowing: $isSelectingSource),
+                                isActive: $isSelectingSource,
+                                label: {
+                                    HStack {
+                                        Text("Source")
+                                        Spacer()
+                                    }.padding()
+                                    .background(Color.white)
+                                    .cornerRadius(3)
+                                })
+                        }
                         
-                    }
-                    
-                    HStack(spacing: 16) {
-                        Image(uiImage: #imageLiteral(resourceName: "annotation_icon")
-                                .withRenderingMode(.alwaysTemplate))
-                            .frame(width: 24, height: 24)
-                            .foregroundColor(.white)
-                        HStack {
-                            Text("Destination")
-                            Spacer()
-                        }.padding()
-                        .background(Color.white)
-                        .cornerRadius(3)
+                        HStack(spacing: 16) {
+                            Image(uiImage: #imageLiteral(resourceName: "annotation_icon")
+                                    .withRenderingMode(.alwaysTemplate))
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.white)
+                            HStack {
+                                Text("Destination")
+                                Spacer()
+                            }.padding()
+                            .background(Color.white)
+                            .cornerRadius(3)
+                            
+                        }
                         
-                    }
+                    }.padding()
+                    .background(Color.blue)
                     
-                }.padding()
-                .background(Color.blue)
+                    Spacer()
+                    DirectionsMapView().edgesIgnoringSafeArea(.bottom)
+                }
                 
-                Spacer()
-                DirectionsMapView().edgesIgnoringSafeArea(.bottom)
+                // status bar cover
+                Spacer().frame(width: UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.window?.frame.width,
+                               height: UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.safeAreaInsets.top)
+                    .background(Color.blue)
+                    .edgesIgnoringSafeArea(.top)
             }
-            
-            // status bar cover
-            Spacer().frame(width: UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.window?.frame.width,
-                           height: UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.safeAreaInsets.top)
-                .background(Color.blue)
-                .edgesIgnoringSafeArea(.top)
+            .navigationBarTitle("Search")
+            .navigationBarHidden(true)
         }
     }
 }
